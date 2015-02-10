9c769995055ff90da5b7cbc43dabcf17291ee774
Allison <allisonmariebrowne@gmail.com>
2015-02-09 21:18:32 -0500

    failing time

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,10 @@
 $("#get-time").on('click', function(e){
   e.preventDefault();
-  console.log("issuing HTTP request")
   $.ajax({
-    url: '/slow_time.json',
+    url: '/failing_time.json',
     dataType: 'json',
     success: function(data){
-        //this logs to the console once HTTP response is recived
-      console.log("successful HTTP response received")
       alert("the time is: " + data.time);
     }
   });
-  //this logs to the console immediately
-  console.log("I can do other things in JavaScript while we wait");
 });
