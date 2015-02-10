adc9f55785b10195f0ba17ee1128af737a8b09bd
Allison <allisonmariebrowne@gmail.com>
2015-02-09 21:14:40 -0500

    comments

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,10 +5,11 @@ $("#get-time").on('click', function(e){
     url: '/slow_time.json',
     dataType: 'json',
     success: function(data){
+        //this logs to the console once HTTP response is recived
       console.log("successful HTTP response received")
       alert("the time is: " + data.time);
     }
   });
-
+  //this logs to the console immediately
   console.log("I can do other things in JavaScript while we wait");
 });
