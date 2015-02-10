6319e50be0ff2d373a1479210ac4899c77f3d310
Allison <allisonmariebrowne@gmail.com>
2015-02-09 21:46:13 -0500

    loads team data on link click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 $("#get-time").on('click', function(e){
   e.preventDefault();
   $.ajax({
-    url: '/failing_time.json',
+    url: '/time.json',
     dataType: 'json',
     success: function(data){
       alert("the time is: " + data.time);
@@ -10,4 +10,40 @@ $("#get-time").on('click', function(e){
       alert("The clock is unavailable due to a server side " + httpStatus);
     }
   });
+});
+
+//builds a table cell for a given value
+function buildCell(value){
+  return $('<td>').html(value);
+}
+
+//builds a table row for a given team
+function buildTeamRow(team){
+  var $row = $('<tr>');
+  $row.append(buildCell(team.rank));
+  $row.append(buildCell(team.name));
+  $row.append(buildCell(team.wins));
+  $row.append(buildCell(team.losses));
+  return $row
+}
+
+function loadStandings(){
+  $.ajax({
+    url: '/teams.json',
+    success: function(data){
+      //clear the contents of the table body
+      $('table tbody').html('');
+      for(var i = 0; i < data.length; i++){
+        $('table tbody').append(buildTeamRow(data[i]));
+      }
+    },
+    error: function(xhr, httpStatus, errorThrown){
+      alert("Something went wrong. Please try again later.");
+    }
+  })
+}
+
+$(function(){
+  //load standings when the page loads
+  loadStandings();
 });
