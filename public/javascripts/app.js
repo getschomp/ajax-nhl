7dd59528de8980b11b0d8329c9a283c442b5cb32
Allison <allisonmariebrowne@gmail.com>
2015-02-09 21:09:41 -0500

    a slower endpoint

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,14 @@
-
 $("#get-time").on('click', function(e){
   e.preventDefault();
-
+  console.log("issuing HTTP request")
   $.ajax({
-    url: '/time.json',
+    url: '/slow_time.json',
     dataType: 'json',
     success: function(data){
-      alert("the time is:" + data.time);
+      console.log("successful HTTP response received")
+      alert("the time is: " + data.time);
     }
   });
 
+  console.log("I can do other things in JavaScript while we wait");
 });
